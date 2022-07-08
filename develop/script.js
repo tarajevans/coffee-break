d976d3790dcbe593470b7a42fd18d877aa443721
Tara Evans <tarajevans@hotmail.com>
2022-07-07 21:30:54 -0400

    setup api call

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,23 @@
+var jokeType="";
+var joke="";
+var punchline="";
+
 fetch("https://v2.jokeapi.dev/joke/Any?safe-mode").then(function(response) {
   response.json().then(function(data) {
-    console.log(data);
+    jokeType=data.type;
+    switch (jokeType) {
+      case "single":
+        joke=data.joke;
+        
+        break;
+      case "twopart":
+        joke=data.setup;
+        punchline=data.delivery;
+
+        break;
+    }
   });
 });
+
+
+//single twopart setup delivery
